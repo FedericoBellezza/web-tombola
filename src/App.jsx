3e2ee04c6970b95b2d8da1247e80081d69f6d7bb
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-12-20 22:04:51 +0100

    basic tombola

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
+  let numbers = [];
+  for (let i = 1; i < 91; i++) {
+    numbers.push({
+      value: i,
+      isActive: false,
+    });
+  }
+  console.log(numbers);
+
+  let newNumbers = [];
+  const callNumber = () => {
+    const number = Math.floor(Math.random() * (90 - 1 + 1) + 1);
+    console.log(number);
+
+    if (newNumbers.includes(number)) callNumber();
+
+    newNumbers = [...calledNumbers, number];
+
+    setCalledNumbers(newNumbers);
+    console.log(newNumbers);
+  };
+
+  const [calledNumbers, setCalledNumbers] = useState([]);
+
   return (
     <>
-      <h1 className="card m-3 p-3 d-inline-block">Hello World</h1>
+      <div className="container">
+        <h1 className="m-3 p-3 d-inline-block">Tombola</h1>
+        <div className="d-flex">
+          <div className="d-flex flex-wrap">
+            {numbers.map((number) => {
+              return (
+                <span
+                  key={number.value}
+                  className={`number-container ${
+                    calledNumbers.includes(number.value) ? "text-bg-dark" : ""
+                  }`}
+                >
+                  {number.value}
+                </span>
+              );
+            })}
+          </div>
+          <div>
+            <button onClick={callNumber} className="btn btn-primary">
+              Chiama numero
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./assets/css/index.css";
 
 // - Importo Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
